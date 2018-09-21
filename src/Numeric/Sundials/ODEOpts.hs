8fb3da57765a534716ee562e83cd0904ac321211
Dominic Steinitz <dominic@steinitz.org>
2018-09-21 14:52:53 +0100

    A global function to update state on finding a root

--- src/Numeric/Sundials/ODEOpts.hs
+++ src/Numeric/Sundials/ODEOpts.hs
@@ -1,6 +1,6 @@
 module Numeric.Sundials.ODEOpts where
 
-import           Data.Word (Word32)
+import           Data.Int (Int32)
 import qualified Data.Vector.Storable as VS
 
 import           Numeric.LinearAlgebra.HMatrix (Vector, Matrix)
@@ -9,24 +9,21 @@ import           Numeric.LinearAlgebra.HMatrix (Vector, Matrix)
 type Jacobian = Double -> Vector Double -> Matrix Double
 
 data ODEOpts = ODEOpts {
-    maxNumSteps :: Word32
+    maxNumSteps :: Int32
   , minStep     :: Double
-  , relTol      :: Double
-  , absTols     :: VS.Vector Double
-  , initStep    :: Maybe Double
-  , maxFail     :: Word32
+  , maxFail     :: Int32
   } deriving (Read, Show, Eq, Ord)
 
 data SundialsDiagnostics = SundialsDiagnostics {
-    aRKodeGetNumSteps               :: Int
-  , aRKodeGetNumStepAttempts        :: Int
-  , aRKodeGetNumRhsEvals_fe         :: Int
-  , aRKodeGetNumRhsEvals_fi         :: Int
-  , aRKodeGetNumLinSolvSetups       :: Int
-  , aRKodeGetNumErrTestFails        :: Int
-  , aRKodeGetNumNonlinSolvIters     :: Int
-  , aRKodeGetNumNonlinSolvConvFails :: Int
-  , aRKDlsGetNumJacEvals            :: Int
-  , aRKDlsGetNumRhsEvals            :: Int
+    odeGetNumSteps               :: Int
+  , odeGetNumStepAttempts        :: Int
+  , odeGetNumRhsEvals_fe         :: Int
+  , odeGetNumRhsEvals_fi         :: Int
+  , odeGetNumLinSolvSetups       :: Int
+  , odeGetNumErrTestFails        :: Int
+  , odeGetNumNonlinSolvIters     :: Int
+  , odeGetNumNonlinSolvConvFails :: Int
+  , dlsGetNumJacEvals            :: Int
+  , dlsGetNumRhsEvals            :: Int
   } deriving Show
 
