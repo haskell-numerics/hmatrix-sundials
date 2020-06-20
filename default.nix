eee0c8371845011d7ecea815e82d336818ba4822
Dominic Steinitz <dominic@steinitz.org>
2020-06-20 11:12:37 +0100

    The missing file

---
+++ default.nix
@@ -0,0 +1,29 @@
+{ mkDerivation, aeson, aeson-pretty, base, bytestring, cassava
+, clock, containers, deepseq, filepath, ghc-prim, hmatrix, inline-c
+, katip, klu, mtl, optparse-applicative, split, stdenv
+, suitesparseconfig, sundials_arkode, sundials_cvode
+, sundials_sunlinsolklu, sundials_sunmatrixsparse, tasty
+, tasty-golden, tasty-hunit, template-haskell, text, vector
+}:
+mkDerivation {
+  pname = "hmatrix-sundials";
+  version = "0.20.0.0";
+  src = ./.;
+  libraryHaskellDepends = [
+    aeson base bytestring containers deepseq ghc-prim hmatrix inline-c
+    katip mtl split template-haskell text vector
+  ];
+  librarySystemDepends = [
+    klu suitesparseconfig sundials_arkode sundials_cvode
+    sundials_sunlinsolklu sundials_sunmatrixsparse
+  ];
+  testHaskellDepends = [
+    aeson aeson-pretty base bytestring filepath hmatrix katip tasty
+    tasty-golden tasty-hunit vector
+  ];
+  benchmarkHaskellDepends = [
+    base bytestring cassava clock hmatrix optparse-applicative
+  ];
+  description = "hmatrix interface to sundials";
+  license = stdenv.lib.licenses.bsd3;
+}
